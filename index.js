45064d0caf7e657e1f50bfd16d362c9e1db3806b
VeraZab <vera.zabeida@gmail.com>
2017-12-29 15:50:45 -0500

    Add initial icons

---
+++ index.js
@@ -0,0 +1,17 @@
+import AngleDownIcon from "mdi-react/ChevronDownIcon";
+import CarretDownIcon from "mdi-react/MenuDownIcon";
+import CarretUpIcon from "mdi-react/MenuUpIcon";
+import CloseIcon from "mdi-react/CloseIcon";
+import CogIcon from "mdi-react/SettingsIcon";
+import LinkIcon from "mdi-react/LinkVariantIcon";
+import QuestionIcon from "mdi-react/HelpCircleIcon";
+
+export {
+  AngleDownIcon,
+  CarretDownIcon,
+  CarretUpIcon,
+  CloseIcon,
+  CogIcon,
+  LinkIcon,
+  QuestionIcon
+};
