9a11a00c7c7f11066c7273cb734abcb8ac028f70
dmt0 <dmitry@plot.ly>
2019-04-04 14:28:38 -0400

    Fixes - couldn't run publish script

--- src/components/TraceTypeWaterfallIcon.js
+++ src/components/TraceTypeWaterfallIcon.js
@@ -3,7 +3,7 @@ import PlotlyIconBase from './IconBase.js';
 
 const TraceTypeWaterfallIcon = (props) => (
     <PlotlyIconBase viewBox="0 0 62.6 59" {...props}>
-        <defs><style>.cls-1{fill:#119dff}.cls-2{fill:#09ffff}.cls-3{fill:none;stroke:#2ebfff;stroke-miterlimit:10}</style></defs><g id="Layer_2" dataName="Layer 2"><g id="Layer_1-2" dataName="Layer 1"><path class="cls-1" transform="rotate(-90 37.255 53.585)" d="M31.84 28.23h10.84v50.69H31.84z"></path><path class="cls-2" transform="rotate(-90 18.155 41.005)" d="M13.27 34.76h9.77v12.5h-9.77z"></path><path class="cls-2" transform="rotate(-90 20.935 17.395)" d="M16.25-3.44h9.37v41.67h-9.37z"></path><path class="cls-1" transform="rotate(-90 33.09 29.35)" d="M28.32 20.67h9.53v17.36h-9.53z"></path><path class="cls-1" transform="rotate(-90 31.35 5.35)" d="M26-25.9h10.7v62.5H26z"></path><path class="cls-3" d="M11.68 59V36M41.26 34V12.71M.5 22V0M23.9 46V24.71"></path></g></g>
+        <g dataName="Layer 2"><g dataName="Layer 1"><path fill="#119dff" d="M11.9 59V48.16h50.69V59z"></path><path fill="#09ffff" d="M11.91 45.89v-9.77h12.5v9.77zM.1 22.08v-9.37h41.67v9.37z"></path><path fill="#119dff" d="M24.41 34.12v-9.53h17.36v9.53zM.1 10.7V0h62.5v10.7z"></path><path fill="none" stroke="#2ebfff" d="M11.68 59V36M41.26 34V12.71M.5 22V0M23.9 46V24.71" strokeMiterlimit="10"></path></g></g>
     </PlotlyIconBase>
 );
 
