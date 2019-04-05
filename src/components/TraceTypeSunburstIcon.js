05afef39b76ad8e058040ad257766baea0f3415b
dmt0 <dmitry@plot.ly>
2019-04-05 13:00:40 -0400

    Remove data-name - unsupported by react

--- src/components/TraceTypeSunburstIcon.js
+++ src/components/TraceTypeSunburstIcon.js
@@ -3,7 +3,7 @@ import PlotlyIconBase from './IconBase.js';
 
 const TraceTypeSunburstIcon = (props) => (
     <PlotlyIconBase viewBox="0 0 60 60" {...props}>
-        <g dataName="Layer 1-2"><circle cx="29.31" cy="31.08" r="14.14" fill="#09ffff"></circle><path d="M29.24 47.68A16.82 16.82 0 0 1 21 45.5l2.17-3.82a12.52 12.52 0 0 0 18.58-10.92h4.41a16.94 16.94 0 0 1-16.92 16.92z" fill="#a6fff8"></path><path d="M20.23 47.09a18.67 18.67 0 0 1 9.34-34.83v18.67M29.57 30.93h18.67a18.67 18.67 0 0 0-18.67-18.67v18.67" fill="#119dff"></path><path d="M19.46 48.5A20 20 0 1 1 49 30.93h-3.82a16.21 16.21 0 1 0-23.91 14.24z" fill="#09ffff"></path><path d="M29.29 31.31h-12.1a12.1 12.1 0 0 1 12.1-12.1z" fill="#09ffff"></path><path d="M31.38 32.49L40 37.63a11.08 11.08 0 0 1-8.41 5.49l-.25-10.63" fill="#119dff"></path><circle cx="29.61" cy="30.35" r="7.32" fill="#fff"></circle><circle cx="29.61" cy="30.35" r="3.1" fill="#119dff"></circle></g>
+        <circle cx="29.31" cy="31.08" r="14.14" fill="#09ffff"></circle><path d="M29.24 47.68A16.82 16.82 0 0 1 21 45.5l2.17-3.82a12.52 12.52 0 0 0 18.58-10.92h4.41a16.94 16.94 0 0 1-16.92 16.92z" fill="#a6fff8"></path><path d="M20.23 47.09a18.67 18.67 0 0 1 9.34-34.83v18.67M29.57 30.93h18.67a18.67 18.67 0 0 0-18.67-18.67v18.67" fill="#119dff"></path><path d="M19.46 48.5A20 20 0 1 1 49 30.93h-3.82a16.21 16.21 0 1 0-23.91 14.24z" fill="#09ffff"></path><path d="M29.29 31.31h-12.1a12.1 12.1 0 0 1 12.1-12.1z" fill="#09ffff"></path><path d="M31.38 32.49L40 37.63a11.08 11.08 0 0 1-8.41 5.49l-.25-10.63" fill="#119dff"></path><circle cx="29.61" cy="30.35" r="7.32" fill="#fff"></circle><circle cx="29.61" cy="30.35" r="3.1" fill="#119dff"></circle>
     </PlotlyIconBase>
 );
 
--- src/components/TraceTypeWaterfallIcon.js
+++ src/components/TraceTypeWaterfallIcon.js
@@ -3,7 +3,7 @@ import PlotlyIconBase from './IconBase.js';
 
 const TraceTypeWaterfallIcon = (props) => (
     <PlotlyIconBase viewBox="0 0 60 60" {...props}>
-        <g dataName="Layer 1-2"><path fill="#119dff" d="M13 18.66h6.27v29.33H13z"></path><path fill="#09ffff" d="M20.59 18.67h5.65v7.23h-5.65zM34.36 11.83h5.42v24.11h-5.42z"></path><path fill="#119dff" d="M27.4 25.9h5.45v10.05H27.4zM40.95 11.83h6.19V48h-6.19z"></path><path fill="none" stroke="#119dff" d="M13 18.53h13.31M27.47 35.65h12.32M34.41 12.06h12.73M20.52 25.61h12.32" strokeMiterlimit="10" strokeWidth=".75"></path></g>
+        <path fill="#119dff" d="M13 18.66h6.27v29.33H13z"></path><path fill="#09ffff" d="M20.59 18.67h5.65v7.23h-5.65zM34.36 11.83h5.42v24.11h-5.42z"></path><path fill="#119dff" d="M27.4 25.9h5.45v10.05H27.4zM40.95 11.83h6.19V48h-6.19z"></path><path fill="none" stroke="#119dff" d="M13 18.53h13.31M27.47 35.65h12.32M34.41 12.06h12.73M20.52 25.61h12.32" strokeMiterlimit="10" strokeWidth=".75"></path>
     </PlotlyIconBase>
 );
 
