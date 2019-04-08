b00a866d66875679c068b24af349c58f87543628
dmt0 <dmitry@plot.ly>
2019-04-08 13:14:14 -0400

    waterfall and sunburst update

--- src/components/PlotWaterfallIcon.js
+++ src/components/PlotWaterfallIcon.js
@@ -3,7 +3,7 @@ import PlotlyIconBase from './IconBase.js';
 
 const PlotWaterfallIcon = (props) => (
     <PlotlyIconBase viewBox="0 0 20 20" {...props}>
-        <path d="M5.67 18H2.78V5h2.89zm-2.51-.38h2.13V5.33H3.16zM8.46 8.46H5.82V5h2.64zM6.2 8.08h1.88V5.33H6.2zM13.89 12.8h-2.55V2h2.55zm-2.17-.38h1.79v-10h-1.79zM11.14 12.8H8.55V8.08h2.59zm-2.21-.38h1.83v-4H8.93zM16.84 18H14.1V2h2.74zm-2.36-.38h2V2.38h-2z"></path><path d="M13.69 2h.62v.38h-.62zM10.95 12.39h.59v.38h-.59zM5.48 4.95H6v.38h-.52zM8.28 8.11h.46v.38h-.46z"></path>
+        <path d="M3.42 5.38h2.5v12.06h-2.5zM6.45 5.38H8.7v2.97H6.45zM11.93 2.57h2.16v9.69h-2.16zM9.16 8.12h2.2v4.13h-2.2zM14.67 2.57h2.35v14.86h-2.35zM5.92 5.38h.52v.22h-.52z"></path><path d="M8.7 8.12h.52v.22H8.7zM11.27 12.03h.78v.22h-.78zM14.09 2.57h.78v.22h-.78z"></path>
     </PlotlyIconBase>
 );
 
