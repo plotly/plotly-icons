51510ae83ebd86ba8af867c09c36ff95e3496fa8
Vera Zabeida <vera.zabeida@gmail.com>
2019-05-21 15:34:09 -0400

    add funnel and funnel area

---
+++ src/components/PlotFunnelIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlotlyIconBase from './IconBase.js';
+
+const PlotFunnelIcon = (props) => (
+    <PlotlyIconBase viewBox="0 0 32.2 29.5" {...props}>
+        <path d="M3.1 3.2h26v5.1h-26z"></path><path fill="currentColor" d="M3.1 8.3l4.5 1.4h17.5l4-1.4h-26z"></path><path d="M7.6 9.7h17.3v5.1H7.6z"></path><path fill="currentColor" d="M7.6 14.8l2 1.3h13.3l2-1.3H7.6z"></path><path d="M9.6 16.1h13.3v5H9.6z"></path><path fill="currentColor" d="M9.6 21.1l2.4 1.5h8.5l2.4-1.5H9.6z"></path><path d="M12 22.6h8.5v3.9H12z"></path>
+    </PlotlyIconBase>
+);
+
+export default PlotFunnelIcon;
---
+++ src/components/TraceTypeFunnelareaIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlotlyIconBase from './IconBase.js';
+
+const TraceTypeFunnelareaIcon = (props) => (
+    <PlotlyIconBase viewBox="0 0 60 60" {...props}>
+        <path fill="#119dff" d="M18.2 35.4l-5.6-10.8h34.8l-5.7 10.8H18.2z"></path><path fill="#0083d3" d="M24.2 48l-5.5-10.8h22.6L35.8 48H24.2z"></path><path fill="#09ffff" d="M12.2 22.8L6.5 12h47l-5.6 10.8H12.2z"></path>
+    </PlotlyIconBase>
+);
+
+export default TraceTypeFunnelareaIcon;
