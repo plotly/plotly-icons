10318f8d3543a53f644415c51477899d56129678
Nicolas Kruchten <nicolas@plot.ly>
2020-01-14 14:47:34 -0500

    adding treemap

---
+++ src/components/PlotTreemapIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlotlyIconBase from './IconBase.js';
+
+const PlotTreemapIcon = (props) => (
+    <PlotlyIconBase viewBox="0 0 60 60" {...props}>
+        <path fill="currentColor" d="M10.11 11.24h19.43v39.13H10.11zM30.58 11.24h19.56v21.49H30.58zM30.58 33.7h19.56v16.66H30.58z"></path>
+    </PlotlyIconBase>
+);
+
+export default PlotTreemapIcon;
---
+++ src/components/TraceTypeTreemapIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlotlyIconBase from './IconBase.js';
+
+const TraceTypeTreemapIcon = (props) => (
+    <PlotlyIconBase viewBox="0 0 60 60" {...props}>
+        <path class="st0" fill="#119DFF" d="M10.1 11.2h19.4v39.1H10.1z"></path><path class="st1" fill="#09FFFF" d="M30.6 11.2h19.6v21.5H30.6z"></path><path class="st2" fill="#43C0FF" d="M30.6 33.7h19.6v16.7H30.6z"></path>
+    </PlotlyIconBase>
+);
+
+export default TraceTypeTreemapIcon;
